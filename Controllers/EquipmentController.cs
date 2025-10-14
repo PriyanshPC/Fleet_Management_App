40447c817a7e1cd2df634a984ecc03c2f02bcd38
Priyansh Chaudhary <Priyanshpc@gmail.com>
2025-10-14 18:03:37 -0400

    Initial Commit

---
+++ Controllers/EquipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fleet_Management_App.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RentalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fleet_Management_App.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
